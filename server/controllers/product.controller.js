dd84cc7401bc74367b5fe5bfd68d4e0574998cb7
Nicolas Chomyn <chomynnicolas2@gmail.com>
2024-06-10 14:35:46 -0400

    prueba nr5

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,14 +1,14 @@
 const { Product } = require("../models/product.model");
 
-module.exports.findAllProducts = (req, res) => {
-  Product.find()
+module.exports.findAllProducts = async (req, res) => {
+  await Product.find()
     .then((product) => res.json(product))
     .catch((err) => res.json(err));
 };
 
-module.exports.createProduct = (req, res) => {
+module.exports.createProduct = async (req, res) => {
   const { title, price, description } = req.body;
-  Product.create({
+  await Product.create({
     title,
     price,
     description,
@@ -17,21 +17,21 @@ module.exports.createProduct = (req, res) => {
     .catch((err) => res.json(err));
 };
 
-module.exports.findProductByid = (req,res)=>{
-  Product.findById({_id:req.params.id})
+module.exports.findProductByid = async(req,res)=>{
+  await Product.findById({_id:req.params.id})
     .then(prod => res.json(prod))
     .catch(err => res.json(err))
 } 
 
 
-module.exports.findAndUpdateById = (req,res)=>{
-  Product.findByIdAndUpdate({_id: req.params.id},req.body,{new:true})
+module.exports.findAndUpdateById = async (req,res)=>{
+  await Product.findByIdAndUpdate({_id: req.params.id},req.body,{new:true})
     .then(newProd => res.json(newProd))
     .catch(err => res.json(err))
 }
 
-module.exports.findAndDeleteById = (req,res)=>{
-  Product.findOneAndDelete({_id:req.params.id})
+module.exports.findAndDeleteById = async (req,res)=>{
+  await Product.findOneAndDelete({_id:req.params.id})
     .then(concl => res.json(concl))
     .catch(err => res.json(err))
 }
